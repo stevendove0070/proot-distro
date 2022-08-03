a236f91821a2d691899beaef9fb7a74e68ecba7a
stevendove <105337158+stevendove0070@users.noreply.github.com>
2022-08-02 19:13:02 -0500

    Add files via upload

---
+++ distro-build/centos.sh
@@ -0,0 +1,33 @@
+# Do not modify this file. All changes will be discarded on the next
+# package update.
+
+# Default value is set by proot-distro script and is equal to the CPU
+# architecture of your device. You can set this to a custom value to
+# force use emulation mode (QEMU user).
+# Valid values are: aarch64, arm, i686, x86_64.
+#DISTRO_ARCH=aarch64
+
+# Name of distribution.
+DISTRO_NAME="Centos"
+
+# Optional comment for distribution.
+DISTRO_COMMENT="Centos 7 Distro"
+
+# How much path components should be stripped when extracting rootfs tarball.
+# The default is "1" which means to omit the root component.
+TARBALL_STRIP_OPT=1
+
+# TARBALL_URL is a Bash associative array containing rootfs URLs for specified
+# CPU architectures. You must specify at least one.
+TARBALL_URL['arm']="https://us.lxd.images.canonical.com/images/centos/7/armhf/default/20220802_07:09/rootfs.tar.xz"
+
+# SHA-256 checksum for the given tarball. You must specify checksum for each
+# defined tarball.
+TARBALL_SHA256['arm']="563d2f1975146114f6ec34930ca2fd4e0c2e7f085102c41ee55ed6ef07eb4692"
+
+# This function defines any additional steps that should be executed during
+# installation. You can use "run_proot_cmd" to execute a given command in
+# proot environment.
+distro_setup() {
+	run_proot_cmd yum update -y
+}
